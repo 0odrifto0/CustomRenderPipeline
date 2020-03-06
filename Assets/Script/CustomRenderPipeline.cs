33ff0d2c3fd06b8699c5f912a34b7a7c33d9f743
drift <>
2020-03-06 22:17:19 +0800

    blinnPhong光照

--- Assets/Script/CustomRenderPipeline.cs
+++ Assets/Script/CustomRenderPipeline.cs
@@ -2,6 +2,7 @@
 using UnityEngine.Rendering;
 using System.Collections.Generic;
 using System;
+using Unity.Collections;
 
 namespace CustomRenderPipline
 {
@@ -44,6 +45,8 @@ namespace CustomRenderPipline
 
                 var cullingResults = renderContext.Cull(ref cullParams);
 
+                SetupMainLightConstants(renderContext, ref cullingResults);
+
                 // render
                 for (int passIndex = 0; passIndex < renderPasses.Count; ++passIndex)
                 {
@@ -71,6 +74,73 @@ namespace CustomRenderPipline
             {
                 return (int)a.depth - (int)b.depth;
             });
+        }
+
+        private void SetupMainLightConstants(ScriptableRenderContext renderContext, ref CullingResults cullingResults)
+        {
+            int index = GetMainLightIndex(cullingResults.visibleLights);
+            if (index == -1)
+                return;
+
+            VisibleLight lightData = cullingResults.visibleLights[index];
+            Vector3 lightPos;
+            Color lightColor;
+
+            if (lightData.lightType == LightType.Directional)
+            {
+                Vector4 dir = -lightData.localToWorldMatrix.GetColumn(2);
+                lightPos = new Vector4(dir.x, dir.y, dir.z, 0.0f);
+            }
+            else
+            {
+                Vector4 pos = lightData.localToWorldMatrix.GetColumn(3);
+                lightPos = new Vector4(pos.x, pos.y, pos.z, 1.0f);
+            }
+
+            // VisibleLight.finalColor already returns color in active color space
+            lightColor = lightData.finalColor;
+
+            var cmd = CommandBufferPool.Get();
+            cmd.SetGlobalVector("_MainLightPosition", lightPos);
+            cmd.SetGlobalVector("_MainLightColor", lightColor);
+            renderContext.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+
+        }
+
+        private int GetMainLightIndex(NativeArray<VisibleLight> visibleLights)
+        {
+            int totalVisibleLights = visibleLights.Length;
+
+            if (totalVisibleLights == 0)
+                return -1;
+
+            Light sunLight = RenderSettings.sun;
+            int brightestDirectionalLightIndex = -1;
+            float brightestLightIntensity = 0.0f;
+            for (int i = 0; i < totalVisibleLights; ++i)
+            {
+                VisibleLight currVisibleLight = visibleLights[i];
+                Light currLight = currVisibleLight.light;
+
+                // Particle system lights have the light property as null. We sort lights so all particles lights
+                // come last. Therefore, if first light is particle light then all lights are particle lights.
+                // In this case we either have no main light or already found it.
+                if (currLight == null)
+                    break;
+
+                if (currLight == sunLight)
+                    return i;
+
+                // In case no shadow light is present we will return the brightest directional light
+                if (currVisibleLight.lightType == LightType.Directional && currLight.intensity > brightestLightIntensity)
+                {
+                    brightestLightIntensity = currLight.intensity;
+                    brightestDirectionalLightIndex = i;
+                }
+            }
+
+            return brightestDirectionalLightIndex;
         }
     }
 }
---
+++ Assets/Shader/BlinnPhongLit.shader
@@ -0,0 +1,85 @@
+﻿Shader "CustomSRP/BlinnPhongLit"
+{
+    Properties
+    {
+        _MainColor ("MainColor", Color) = (1,1,1,1)
+        _MainTex ("Texture", 2D) = "white" {}
+        _NormalMap ("Bump", 2D) = "Bump" {}
+        _SpecularFactor ("Specular Factor", float) = 32
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" "Queue"="Geometry"}
+        LOD 200
+
+        Pass
+        {
+     		Tags { "LightMode" = "ForwardBase" }
+            HLSLPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+			#include "Assets/ShaderLibrary/Core.hlsl"
+
+            struct appdata
+            {
+                float3 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+                float3 normal : NORMAL;
+                float4 tangent : TANGENT;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+                float3 viewDir : TEXCOORD1;
+                float3 lightDir : TEXCOORD2;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            half4 _MainColor;
+            sampler2D _NormalMap;
+            float4 _NormalMap_ST;
+            float _SpecularFactor;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = TransformObjectToHClip(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+
+                float3x3 objectToTangent = CreateTangentToWorld(v.normal, v.tangent, v.tangent.w);
+                
+                float3 objectCameraPos = TransformWorldToObject(_WorldSpaceCameraPos);
+                float3 objectViewDir = normalize(objectCameraPos - v.vertex);
+                float3 objectLightDir = TransformWorldToObjectDir(normalize(_MainLightPosition.xyz));
+                o.viewDir = mul(objectToTangent, objectViewDir);
+                o.lightDir = mul(objectToTangent, objectLightDir);
+                
+                return o;
+            }
+
+            half4 frag (v2f i) : SV_Target
+            {
+                float3 normal = normalize(UnpackNormal(tex2D(_NormalMap, i.uv)).xyz * 2);
+
+                half4 albedo = tex2D(_MainTex, i.uv) * _MainColor;
+
+                float3 lightDir = i.lightDir;
+                // diffuse
+                float diffuse = max(dot(lightDir, normal), 0);
+
+                // specular
+                float3 viewDir = i.viewDir;
+                float3 halfDir = normalize(lightDir + viewDir);
+                float specular = pow(max(dot(halfDir, normal), 0), _SpecularFactor);
+                
+                half4 finalColor = albedo + diffuse * albedo + specular*_MainLightColor;
+                return finalColor;
+            }
+
+            ENDHLSL
+        }
+    }
+}
